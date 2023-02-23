490f702d7a40da9c3d61671ae3dac23a433ad262
Sean Showalter <sean@showalter.design>
2023-02-23 21:20:50 +0000

    Test passed.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,6 @@
 public class Application {
     /**
-     * This class contains a main method that allows you to manually test the CustomException lab.
+     * This class contains a main method that allows you to manually test the CustomException lab. 
      * No changes you make here affect the test case results. You must still run & pass the test cases.
      * All implementation code should be written in the Lab class.
      *
